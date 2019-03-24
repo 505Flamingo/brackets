cc03e5b015fd10b210538b8f953a600a370ca0cf
Lidiya Gorohova <helga2011@yandex.ru>
2019-03-25 01:23:14 +0300

    Add function that check balanced brackets (not full)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsStorage = [];
+  let openBrackets = ['(','[','{','|','1','3','5','7','8'];
+  let closeBrackets = [')',']','}','|','2','4','6','7','8'];
+  for (i = 0; i < str.length; i++ ) {
+    if (openBrackets.includes(str[i])) {
+    bracketsStorage.push(str[i]);    
+    } 
+    if (closeBrackets.includes(str[i]) && bracketsStorage.length !== 0) {
+      if (closeBrackets.indexOf(str[i]) == openBrackets.indexOf(bracketsStorage.pop()) ) {
+        if (bracketsStorage.length === 0) {
+          return true;
+        } 
+      } else {
+        return false;
+      }
+      } else {
+        return false;
+      }
+    }
 }
